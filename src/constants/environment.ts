be19d6eb85044eb69613515371ec5dbf1d857257
tanatut12 <tanatut12@gmail.com>
2024-12-08 17:21:30 +0700

    set env

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,13 +1,8 @@
-
-
-
-
 export const env = {
   API: {
-    API_URL: import.meta.env.NEST_BACKEND_URL,
-    // API_KEY: process.env.API_KEY,
+    API_URL: import.meta.env.VITE_NEST_BACKEND_URL,
   },
   FRONT: {
-    FRONT_BASE_URL: import.meta.env.FRON_BASE_URL,
+    FRONT_BASE_URL: import.meta.env.VITE_FRON_BASE_URL,
   },
 };
