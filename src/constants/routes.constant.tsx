12e275e1ed7ce368b341f22f76780cc05401370d
thanakorn <thanakorn@thanakorns-MacBook-Air.local>
2024-12-16 18:43:09 +0700

    create layout navbar & footer

--- src/constants/routes.constant.tsx
+++ src/constants/routes.constant.tsx
@@ -1,9 +1,14 @@
 import AuthContainer from '../containers/Auth/AuthContainer';
+import LayoutScreen from '../containers/Layout/LayoutScreen';
 import RegisterContainer from '../containers/register/RegisterContainer';
 
 const routes = [
   { path: '/register', element: <RegisterContainer /> },
-  { path: '', element: <AuthContainer /> },
+  {
+    path: '',
+    element: <LayoutScreen />,
+    children: [{ path: '/', element: <AuthContainer /> }],
+  },
 ];
 
 export default routes;
